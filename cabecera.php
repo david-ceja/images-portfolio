610251a74ac9a5bcbb6dae88d32ce38389ee1dfa
David Ceja <david.ceja@accend.com.mx>
2022-03-23 09:43:18 -0600

    Add link to yyy in header

--- cabecera.php
+++ cabecera.php
@@ -7,7 +7,7 @@
 <body>
     <!-- bs5-grid-container -->
     <div class="container">
-        <a href="index.php">Inicio</a> | <a href="portfolio.php">Administrar Portafolio</a> | <a href=<?php echo $_SESSION?'"logout.php">logout':'"login.php">login'?></a>
+        <a href="index.php">Inicio</a> | <a href="portfolio.php">Administrar Portafolio</a> | <a href=<?php echo $_SESSION?'"logout.php">logout':'"login.php">login'?></a> | <a href="yyy.php">YYY</a>
 
     <!-- QUITAMOS EL CIERRE DEL DIV Y LO PONEMOS AL INICIO DEL FOOTER -->
     <!-- DE MODO QUE TODO LO QUE VAYA EN EL CUERPO DE LA PÁGINA, ESTÉ ENCERRADO EN ESTE CONTENEDOR -->
